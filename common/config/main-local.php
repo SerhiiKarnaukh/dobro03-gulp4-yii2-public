917c44068512fe379d317baf3d7de5e8db1a7444
Serhii Karnaukh <serhiikarnaukh1984@gmail.com>
2021-10-06 14:59:01 +0300

    Packages updated

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=dobro03',
             'username' => 'root',
-            'password' => 'root',
+            'password' => '',
             'charset' => 'utf8',
             'tablePrefix' => 'dbr_',
         ],
